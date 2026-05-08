f1830fe3f87540b8168847e86ecae756eaa88d0a
Inc911 <103965431+Inc911@users.noreply.github.com>
2026-05-08 12:53:30 +0300

    updated

--- GrokLM/2.NBC/nbc.py
+++ GrokLM/2.NBC/nbc.py
@@ -26,107 +26,82 @@ def xl_2_list(file):
     return data
 
 
+def get_X(file):
+    data = []  # Пустой список, для сохранения извлеченных из файла данные
+    workbook = load_workbook(file)
+    page = workbook.active
+    for row in page.iter_rows(values_only=True):
+        if (row[0] == None) & (
+            row[4] == "?"
+        ):  # находим объект Х по пустому ID и символу ? в поле Класса
+            item = {
+                "ID": row[0],
+                "x1": row[1],
+                "x2": row[2],
+                "x3": row[3],
+                "Class": row[4],
+            }  # {'ID': None, 'x1': 'Мультикам', 'x2': 'АК-74', 'x3': 'Бег', 'Class': '?'}
+            data.append(item)
+
+    return data
+
+
+def find_classes(list):
+    c_n_names = []  # the list of classes' names
+    for obj in list:  # obj - словарь объекта
+        if obj["Class"] not in c_n_names:  # если Свой/Враг нет в классе
+            c_n_names.append(obj["Class"])  # то добавляем его в список
+        else:
+            continue
+
+    # ПОЛУЧАЕМ СПИСОК СПИСКОВ ОБЪЕКТОВ ПО КЛАССАМ
+    c_n_list = []  # Список объектов класса С
+    for c in c_n_names:  # с - Класс (Свой/Враг)
+        classes = []  # список названий классов
+
+        for ob in list:  # ob - словарь объекта
+            if (
+                ob["Class"] == c
+            ):  # Если значение класса из списка соответсвует текущему значнеию в словаре классов
+                classes.append(ob)  # добавляем его в список
+            c_n_list.append(classes)
+    return c_n_list
+
+
+def calc_priori_P(in_data):
+    total_len = len(in_data)
+    for obj in find_classes(in_data):
+        print(
+            f"Апприорная вероятность класса C{find_classes(in_data).index(obj) + 1}:",
+            len(obj) / total_len,
+        )
+
+def calc_cond_P(X,in_data):
+    # X = {'ID': None, 'x1': 'Мультикам', 'x2': 'АК-74', 'x3': 'Бег', 'Class': '?'}
+    # in_data = [1[8{5}], 2[8{5}]]
+    dict = {}
+    for x in X: # p - параметр объекта Х
+        count = 0
+        for c in in_data: # c - список объектов класса 
+            for obj in c: # obj - объект класса с
+                for p in obj: # p - параметр объекта класса с
+                    if x == p: # сравненение параметра объекта Х и параметра объекта класса 
+                        count += 1
+                        dict[x] = count
+        print(dict)
+                        
+
+
+
+dataset = xl_2_list(file_path)
+obj_x = get_X(file_path)
+sorted_ds = find_classes(dataset)
+# calc_priori_P(dataset)
+calc_cond_P(obj_x, sorted_ds)
+
+
 # [{'ID': 1, 'x1': 'Цифра', 'x2': 'АК-74', 'x3': 'Патруль', 'Class': 'Свой'}
 #  {'ID': 2, 'x1': 'Цифра', 'x2': 'АК-74', 'x3': 'Бег',     'Class': 'Свой'}
 # ...]
 
-
-def find_elem(in_data):
-    lit = []
-    for i in in_data:  #  i - словарь
-        for el in i.values():  # el - входящее слово, i.values() - список значений
-            if el in list:
-                continue
-            else:
-                lit.append(el)
-    for n in lit:
-        print(n)
-        
-# def find_elem(el, in_data):
-#     list = {}
-#     count = 0
-#     for i in in_data:  #  i - словарь
-#         if el in i.values():  # el - входящее слово, i.values() - список значений
-#             count += 1
-#             list.update({el: count})
-#             continue
-#         else:
-#             list[el] = count
-#     return list
-
-
 # print(xl_2_list(file_path))
-find_elem(xl_2_list(file_path))
-# print(find_elem("Свой", xl_2_list(file_path)))
-
-#     if line.startswith("№"):  # Если чтрока начинается с "ID"
-#         is_reading = True  # то флаг переключается в True
-#         if is_reading:  # если флаг включен и строка пустая
-#             break  # то прервать цикл
-
-#         if is_reading:  # При включении флага
-#             row = clean_line.split()  # Разделяем строку по пробельным символам на ряды
-#             if len(row) >= 4:  # Если количество рядов в строке больше либо равно 4,
-#                 item = {  # то формируем словарь из ключей и значений
-#                     "ID": int(row[0]),
-#                     "x1": float(row[1]),
-#                     "x2": float(row[2]),
-#                     "Class": row[3],
-#                 }
-#                 data.append(item)  # и добавляем словарь в список словарей
-
-# print(data)
-
-
-# def process_email(text):
-#     text = text.lower()
-#     return list(set(text.split()))
-
-
-# emails["words"] = emails["text"].apply(process_email)
-# sum(emails["spam"]) / len(emails)
-
-# model = {}  # создаем пучстой словарь
-
-# for (
-#     index,
-#     row,
-# ) in (
-#     emails.iterrows()
-# ):  # Перебираем кортежи из индексов (номеров) строк и их содержимых
-#     # print(row)
-#     for word in row["words"]:  # перебираем содержимое строк в столбце words
-#         if word not in model:  # если слова нет в словаре
-#             model[word] = {
-#                 "spam": 1,
-#                 "ham": 1,
-#             }  # то добавляем в словарь model ключ с содержимым word и значением {'spam' : 1, 'ham': 1} Сглаживание Лапласа, чтобы избежать нулевого количества и случайно не разделить на 0
-#         if word in model:
-#             if row["spam"]:  # если в стобце spam - 1 то верется True, если 0, то False
-#                 model[word][
-#                     "spam"
-#                 ] += 1  # если в стобце spam - 1 то значение spam увеличивается на 1
-#             else:
-#                 model[word][
-#                     "ham"
-#                 ] += 1  # если в стобце spam - 0 то значение ham увеличивается на 1
-# # print(f'{model['sale']}')
-
-
-# def predict_naive_bayes(email):
-#     total = len(emails)
-#     num_spam = sum(emails["spam"])
-#     num_ham = total - num_spam
-#     email = email.lower()
-#     words = set(email.split())
-#     spams = [1.0]
-#     hams = [1.0]
-#     for word in words:
-#         if word in model:
-#             spams.append(model[word]["spam"] / num_spam * total)
-#             hams.append(model[word]["ham"] / num_ham * total)
-#     prod_spams = np.long(np.prod(spams) * num_spam)
-#     prod_hams = np.long(np.prod(hams) * num_ham)
-#     return prod_spams / (prod_spams + prod_hams)
-
-#  print(predict_naive_bayes("dffsaf asdfvfbdvs asfvbd sefaewbnl, gbrn,"))
