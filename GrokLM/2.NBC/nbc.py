a651cd13026b0bdf6d77852018660e253888fe94
Inc911 <103965431+Inc911@users.noreply.github.com>
2026-05-10 11:52:45 +0300

    pre review version

--- GrokLM/2.NBC/nbc.py
+++ GrokLM/2.NBC/nbc.py
@@ -6,43 +6,37 @@ from collections import Counter
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_dir, "dataset.xlsx")
-is_reading = False  # Флаг "читаемости", который включается после начала таблицы
-
 
 def xl_2_list(file):
     data = []  # Пустой список, для сохранения извлеченных из файла данные
     workbook = load_workbook(file)
     page = workbook.active
     for row in page.iter_rows(values_only=True):
-        if isinstance(row[0], int):
+        # if isinstance(row[0], int):
+        item = {
+            "ID": row[0],
+            "x1": row[1],
+            "x2": row[2],
+            "x3": row[3],
+            "Class": row[4],
+        }  # {'ID': 1, 'x1': 'Цифра', 'x2': 'АК-74', 'x3': 'Патруль', 'Class': 'Свой'}
+        
+        data.append(item)
+    
+    return data
 
-            item = {
-                "ID": row[0],
-                "x1": row[1],
-                "x2": row[2],
-                "x3": row[3],
-                "Class": row[4],
-            }  # {'ID': 1, 'x1': 'Цифра', 'x2': 'АК-74', 'x3': 'Патруль', 'Class': 'Свой'}
+def get_params(in_data):
+    data = []
+    for item in in_data:
+        if isinstance(item["ID"], int):
             data.append(item)
     return data
 
-
-def get_X(file):
-    workbook = load_workbook(file)
-    page = workbook.active
-    for row in page.iter_rows(values_only=True):
-        if (row[0] == None) & (
-            row[4] == "?"
-        ):  # находим объект Х по пустому ID и символу ? в поле Класса
-            item = {
-                "ID": row[0],
-                "x1": row[1],
-                "x2": row[2],
-                "x3": row[3],
-                "Class": row[4],
-            }  # {'ID': None, 'x1': 'Мультикам', 'x2': 'АК-74', 'x3': 'Бег', 'Class': '?'}
-
-    return item
+def get_X(in_data):
+    for item in in_data:
+        if item["Class"] == "?":
+            print (item)
+            return item
 
 
 def find_classes(list):
@@ -117,24 +111,29 @@ def calc_cond_P(X, in_data):
 def calc_Bayes_P(priori_P_list, cond_P_dict):
     #  priori_P_list = {'Свой': 0.5, 'Враг': 0.5}
     #  cond_P_dict = {'Свой': {'АК-74': 0.75, 'Бег': 0.25, 'Мультикам': 0.25}, 'Враг': {'Мультикам': 0.5, 'Бег': 0.5, 'АК-74': 0.25}}
-    bayes_P_list = {}
+    bayes_P_dict = {}
+    full_P = 0
     for c in priori_P_list:  # {'Свой': 0.5, 'Враг': 0.5}
         bayes_P = 1 * priori_P_list[c]
-        print(c)
         # print(priori_P_list[c])
         # print(cond_P_dict[c])
         for b in cond_P_dict[c]:
             bayes_P *= cond_P_dict[c][b]
-        print(bayes_P)
-    # return None
+        full_P += bayes_P
+        bayes_P_dict[c] = bayes_P
+
+    for b in bayes_P_dict:
+        bayes_P_dict[b] = round(bayes_P_dict[b] / full_P * 100, 2)
+    return bayes_P_dict
 
 
 dataset = xl_2_list(file_path)
-obj_x = get_X(file_path)
-sorted_ds = find_classes(dataset)
+params = get_params(dataset)
+obj_x = get_X(dataset)
+sorted_ds = find_classes(params)
 priori_P = get_priori_P(sorted_ds)
 cond_P = calc_cond_P(obj_x, sorted_ds)
-# print("\nАПРИОРНЫЕ ВЕРОЯТНОСТИ:\n\t", priori_P)
-# print("\nУСЛОВНЫЕ ВЕРОЯТНОСТИ:\n\t", cond_P)
-
-calc_Bayes_P(priori_P, cond_P)
+bayes_P = calc_Bayes_P(priori_P, cond_P)
+print("\nАПРИОРНЫЕ ВЕРОЯТНОСТИ:\n\t", priori_P)
+print("\nУСЛОВНЫЕ ВЕРОЯТНОСТИ:\n\t", cond_P)
+print("\nВЕРОЯТНОСТЬ ПО БАЙЕСУ:\n\t", bayes_P)
