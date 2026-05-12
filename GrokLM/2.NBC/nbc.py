cefdd94bbb3797742a38909ef93f5fe19d17f118
Inc911 <103965431+Inc911@users.noreply.github.com>
2026-05-12 07:57:32 +0300

    rewrited nbc. py. Counted apriori_P in new nbc.py

--- GrokLM/2.NBC/nbc.py
+++ GrokLM/2.NBC/nbc.py
@@ -10,7 +10,6 @@ def xl_2_list(file):
     workbook = load_workbook(file)
     page = workbook.active
     for row in page.iter_rows(values_only=True):
-        # if isinstance(row[0], int):
         item = {
             "ID": row[0],
             "x1": row[1],
@@ -24,109 +23,74 @@ def xl_2_list(file):
     return data
 
 
-def get_params(in_data):
+def get_x_list(full_dataset):
     data = []
-    for item in in_data:
+    for item in full_dataset:
         if isinstance(item["ID"], int):
             data.append(item)
     return data
 
 
-def get_X(in_data):
-    for item in in_data:
+def get_X(full_dataset):
+    for item in full_dataset:
         if item["Class"] == "?":
             return item
 
 
-def find_classes(list):
-    c_n_names = []  # the list of classes' names
-    for obj in list:  # obj - словарь объекта
-        if obj["Class"] not in c_n_names:  # если Свой/Враг нет в классе
-            c_n_names.append(obj["Class"])  # то добавляем его в список
-        else:
-            continue
-
-    # ПОЛУЧАЕМ СПИСОК СПИСКОВ ОБЪЕКТОВ ПО КЛАССАМ
-    c_n_list = []  # Список объектов класса С
-    for c in c_n_names:  # с - Класс (Свой/Враг)
-        classes = []  # список названий классов
-
-        for ob in list:  # ob - словарь объекта
-            if (
-                ob["Class"] == c
-            ):  # Если значение класса из списка соответсвует текущему значнеию в словаре классов
-                classes.append(ob)  # добавляем его в список
-        c_n_list.append(classes)
-
-    return c_n_list
+def get_y(x_list):
+    CLASSES = []
+    for x in x_list:
+        if x["Class"] not in CLASSES:
+            CLASSES.append(x["Class"])
+    return CLASSES
 
 
-def get_priori_P(sorted_ds):  #
-    priori_P = {}
-    classes_count = len(sorted_ds)  # 2
-    for obj in sorted_ds:
-        priori_P[obj[1]["Class"]] = 1 / classes_count
-    return priori_P
+def sort_dataset(x_list, y_list):
+    sorted_ds = []
+    for y in y_list:
+        y_n_list = []
+        for x in x_list:
+            if x["Class"] == y:
+                y_n_list.append(x)
+        sorted_ds.append(y_n_list)
+    return sorted_ds
 
 
-def calc_cond_P(X, in_data):
-    # X = {'ID': None, 'x1': 'Мультикам', 'x2': 'АК-74', 'x3': 'Бег', 'Class': '?'}
-    # in_data = [1[4{5}], 2[4{5}]]
-    cond_P_list = []
-    cond_P_dict = {}
-    for c in in_data:  # c - список объектов класса
-        dict = []
-        duples = {}
+def calc_priori_P(sorted_ds, x_count):
+    priori_P_list = {}
+    for y_n in sorted_ds:
+        for x in y_n:
+            priori_P_list[x["Class"]] = round(len(y_n) / x_count, 2)
 
-        for obj in c:  # obj - объект класса с
-            for x in X:  # p - параметр объекта Х
-                for p in obj:  # p - параметр объекта класса с
-                    if X.get(x) == obj.get(
-                        p
-                    ):  # сравненение параметра объекта Х и параметра объекта класса
-                        dict.append(X.get(x))
-        for el in dict:
-            if duples.get(el):
-                duples[el] += 1
-            else:
-                duples[el] = 1
+    return priori_P_list
 
-        for d in duples:
-            duples[d] /= len(c)
-        cond_P_list.append(duples)
-
-        for cp in cond_P_list:
-            cond_P_dict[c[1]["Class"]] = cp
-
-    return cond_P_dict
+# def calc_cond_P()
 
 
-def calc_Bayes_P(priori_P_list, cond_P_dict):
-    #  priori_P_list = {'Свой': 0.5, 'Враг': 0.5}
-    #  cond_P_dict = {'Свой': {'АК-74': 0.75, 'Бег': 0.25, 'Мультикам': 0.25}, 'Враг': {'Мультикам': 0.5, 'Бег': 0.5, 'АК-74': 0.25}}
-    bayes_P_dict = {}
-    full_P = 0
-    for c in priori_P_list:  # {'Свой': 0.5, 'Враг': 0.5}
-        bayes_P = 1 * priori_P_list[c]
-        # print(priori_P_list[c])
-        # print(cond_P_dict[c])
-        for b in cond_P_dict[c]:
-            bayes_P *= cond_P_dict[c][b]
-        full_P += bayes_P
-        bayes_P_dict[c] = bayes_P
+list_from_file = xl_2_list(file_path)
+print("\n\tПОЛНЫЙ ДАТАСЕТ ИЗ ФАЙЛА:", len(list_from_file), "\n", list_from_file)
 
-    for b in bayes_P_dict:
-        bayes_P_dict[b] = round(bayes_P_dict[b] / full_P * 100, 2)
-    return bayes_P_dict
+x_list = get_x_list(list_from_file)
+print("\n\tИЗВЕСТНЫЕ ОБЪЕКТЫ:", len(x_list), "\n", x_list)
+
+X = get_X(list_from_file)
+print("\n\tИСКОМЫЙ ОБЪЕКТ:", len(X), "\n", X)
+
+y_list = get_y(x_list)
+print("\n\tКЛАССЫ ДАТАСЕТА:", len(y_list), "\n", y_list)
 
 
-dataset = xl_2_list(file_path)  # получаем список из файла
-params = get_params(dataset)  # из списка получаем объекты с параметрами
-obj_x = get_X(dataset)  # из списка получаем искомый объект
-sorted_ds = find_classes(params)  # сортируем список объектов с параметрами по классам
-priori_P = get_priori_P(sorted_ds)  # из сортированного сптска получаем априорные вероятности
-cond_P = calc_cond_P(obj_x, sorted_ds)  # Получаем условные вероятности
-bayes_P = calc_Bayes_P(priori_P, cond_P)  # Считаем вероятности по Байесу
-print("\nАПРИОРНЫЕ ВЕРОЯТНОСТИ:\n\t", priori_P)
-print("\nУСЛОВНЫЕ ВЕРОЯТНОСТИ:\n\t", cond_P)
-print("\nВЕРОЯТНОСТЬ ПО БАЙЕСУ:\n\t", bayes_P)
+sorted_ds = sort_dataset(x_list, y_list)
+print("\n\tОТСОРТИРОВАННЫЙ ДАТАСЕТ:", len(sorted_ds), "\n", sorted_ds)
+
+priori_P = calc_priori_P(sorted_ds, len(x_list))
+print("\n\tАПРИОРНЫЕ ВЕРОЯТНОСТИ:", len(priori_P), "\n", priori_P)
+
+
+# TODO 1. ИЗВЛЕЧЬ ДАТАСЕТ ИЗ ФАЙЛА
+# TODO 2. ИЗВЛЕЧЬ ИЗ ДАТАСЕТА ИЗВЕСТНЫЕ ОБЪЕКТЫ
+# TODO 3. ИЗВЛЕЧЬ ИЗ ДАТАСЕТА НЕИЗВЕСТНЫЙ ОБЪЕКТ
+# TODO 4. ПОЛУЧИТЬ КЛАССЫ ИЗ ИЗВЕСТНОГО ДАТАСЕТА
+# TODO 5. ОТСОРТИРОВАТЬ ИЗВЕСТНЫЙ ДАТАСЕТ ПО КЛАССАМ
+# TODO 6. РАССЧИТАТЬ АППРИОРНЫЕ ВЕРОЯТНОСТИ
+# TODO 7. РАССЧИТАТЬ УСЛОВНЫЕ ВЕРОЯТНОСТИ
